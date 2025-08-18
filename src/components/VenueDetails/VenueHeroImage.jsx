54d35ba772d1fad57842077b9d525897c95e93f5
Kuba Klin <poczta@kubaklin.pl>
2025-08-18 19:16:37 +0200

    feat: favorite indicator in venue details

--- src/components/VenueDetails/VenueHeroImage.jsx
+++ src/components/VenueDetails/VenueHeroImage.jsx
@@ -1,5 +1,10 @@
 import { StyledBox } from './VenueHeroImage.styles';
+import { FavoriteButton } from '../VenueList/VenueCard/FavoriteButton';
 
 export const VenueHeroImage = ({ venue }) => {
-  return <StyledBox backgroundImage={venue?.galleryImages[0]} />;
+  return (
+    <StyledBox backgroundImage={venue?.galleryImages[0]}>
+      <FavoriteButton venueId={venue.venueId || venue.id} />
+    </StyledBox>
+  )
 };
