7264b4531b3eebf4c377a2c1f4427b3d6f6209d2
Kuba Klin <poczta@kubaklin.pl>
2025-08-18 19:18:25 +0200

    feat: add slider to venu details

--- src/components/VenueDetails/VenueHeroImage.jsx
+++ src/components/VenueDetails/VenueHeroImage.jsx
@@ -1,9 +1,11 @@
 import { StyledBox } from './VenueHeroImage.styles';
 import { FavoriteButton } from '../VenueList/VenueCard/FavoriteButton';
+import {ImageSlider} from "../VenueList/VenueCard/ImageSlider/ImageSlider.jsx";
 
 export const VenueHeroImage = ({ venue }) => {
   return (
-    <StyledBox backgroundImage={venue?.galleryImages[0]}>
+    <StyledBox>
+      <ImageSlider images={venue.galleryImages} venueName={venue.name} />
       <FavoriteButton venueId={venue.venueId || venue.id} />
     </StyledBox>
   )
